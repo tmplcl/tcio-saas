a44a5f5546a871b7d25d052acea44f8ae70c1019
Timur Philip Cöl <timur.philip.coel@gmail.com>
2022-01-16 14:00:54 +0100

    Add basic pipeline

--- apps/capp/bin/capp.ts
+++ apps/capp/bin/capp.ts
@@ -1,9 +1,16 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import {CappStack} from '../lib/capp-stack';
+import {PipelineStack} from "aws-cdk-lib/aws-codepipeline-actions/test/integ.pipeline-ecs-separate-source.lit";
 
 const app = new cdk.App();
 new CappStack(app, 'CappStack', {
+    env: {
+        account: '834067345082',
+        region: 'eu-central-1'
+    }
+});
+new PipelineStack(app, 'PipelineSTack', {
     env: {
         account: '834067345082',
         region: 'eu-central-1'
